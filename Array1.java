62f0abc26d694c5b9e49f87b7abdd99d355a9e2c
vivek vardhan . M <84318895+vikkyvivek2002@users.noreply.github.com>
2021-07-05 16:45:25 +0530

    Add files via upload

---
+++ Array1.java
@@ -0,0 +1,13 @@
+public class Array1
+ {
+
+	public static void main (String[] args)
+	{
+		int a[] = {10,20,30,40,50};
+		for(int i= 0; 1<5 ; i++)
+		{
+			System.out.println(a[i]);
+		}
+					
+	}
+}
